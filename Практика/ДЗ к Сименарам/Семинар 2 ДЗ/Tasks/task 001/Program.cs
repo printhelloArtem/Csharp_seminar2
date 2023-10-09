9b7f34965bf742c8cd01ed3a56519ea6fa93a3df
Artem Zhurba <art201200@yandex.ru>
2023-10-09 22:27:21 +0300

    добавил задатча тест

--- Практика/ДЗ к Сименарам/Семинар 2 ДЗ/Tasks/task 001/Program.cs
+++ Практика/ДЗ к Сименарам/Семинар 2 ДЗ/Tasks/task 001/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ //задача 2
+Console.WriteLine("Введите первое число: ");
+double firstScore1  = Convert.ToDouble (Console.ReadLine ());
+Console.WriteLine("Введите второе число: ");
+double secondScore2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+    if(firstScore1 < secondScore2)
+    Console.WriteLine(firstScore1 + " Минимальное число");
+    Console.WriteLine (secondScore2 + " максимальное число");
